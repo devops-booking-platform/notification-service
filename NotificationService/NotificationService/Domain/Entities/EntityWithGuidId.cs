92951fc3f19c8fe9f091b527258786e71c0c654f
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-12-09 19:39:27 +0100

    feat(config): Added entity configs

---
+++ NotificationService/NotificationService/Domain/Entities/EntityWithGuidId.cs
@@ -0,0 +1,6 @@
+﻿namespace NotificationService.Domain.Entities;
+
+public abstract class EntityWithGuidId
+{
+    public Guid Id { get; set; } = Guid.NewGuid();
+}
---
+++ NotificationService/NotificationService/Domain/Entities/Notification.cs
@@ -0,0 +1,24 @@
+﻿namespace NotificationService.Domain.Entities;
+
+public class Notification : EntityWithGuidId
+{
+    public const int MessageMaxlength = 2056;
+    public Guid UserId { get; set; }
+    public NotificationType NotificationType { get; set; }
+    public DateTimeOffset CreatedOn { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public bool Read { get; set; }
+
+
+    public static Notification Create(Guid userId, NotificationType notificationType, string message)
+        => new()
+        {
+            UserId = userId,
+            NotificationType = notificationType,
+            Message = message,
+            Read = false,
+            CreatedOn = DateTimeOffset.UtcNow
+        };
+    
+    public void MarkRead() => Read = true;
+}
