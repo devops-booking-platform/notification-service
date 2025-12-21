6e26e5a2b66c0c50dcb872a277897765298ce727
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-12-21 12:02:43 +0100

    feat(notif): Added aditional fields

--- NotificationService/NotificationService/Common/Events/IIntegrationEvent.cs
+++ NotificationService/NotificationService/Common/Events/IIntegrationEvent.cs
@@ -4,9 +4,25 @@ public interface IIntegrationEvent
 {
 }
 
-public record HostRatedIntegrationEvent(Guid HostId) : IIntegrationEvent;
-public record AccommodationRatedIntegrationEvent(Guid HostId, Guid AccommodationId) : IIntegrationEvent;
+public record HostRatedIntegrationEvent(Guid HostId, string GuestUsername, int Rating) : IIntegrationEvent;
 
-public record ReservationCreatedIntegrationEvent(Guid HostId, Guid ReservationId) : IIntegrationEvent;
-public record ReservationCanceledIntegrationEvent(Guid HostId, Guid ReservationId) : IIntegrationEvent;
-public record ReservationRespondedIntegrationEvent(Guid GuestId, Guid ReservationId, bool IsApproved) : IIntegrationEvent;
+public record AccommodationRatedIntegrationEvent(Guid HostId, Guid AccommodationId, string GuestUsername, int Rating)
+    : IIntegrationEvent;
+
+public record ReservationCreatedIntegrationEvent(
+    Guid HostId,
+    Guid ReservationId,
+    string AccommodationName,
+    DateTimeOffset StartDate,
+    DateTimeOffset EndDate,
+    string GuestUsername) : IIntegrationEvent;
+
+public record ReservationCanceledIntegrationEvent( Guid HostId,
+    Guid ReservationId,
+    string AccommodationName,
+    DateTimeOffset StartDate,
+    DateTimeOffset EndDate,
+    string GuestUsername) : IIntegrationEvent;
+
+public record ReservationRespondedIntegrationEvent(Guid GuestId, Guid ReservationId, string AccommodationName, bool IsApproved)
+    : IIntegrationEvent;
