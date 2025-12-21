35bb2ee7c0af1654f73cd7797546674bd10ca837
Nebojša Vuga <nebojsavuga01@gmail.com>
2025-12-21 12:44:02 +0100

    feat(notif): Added full signalR logic

--- NotificationService/NotificationService/IntegrationEvents/Handlers/ReservationCanceledIntegrationEventHandler.cs
+++ NotificationService/NotificationService/IntegrationEvents/Handlers/ReservationCanceledIntegrationEventHandler.cs
@@ -1,15 +1,50 @@
-﻿using NotificationService.Common.Events;
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using NotificationService.Common.Events;
+using NotificationService.Common.Hubs;
+using NotificationService.Domain.DTOs;
+using NotificationService.Domain.Entities;
+using NotificationService.Repositories.Interfaces;
 
 namespace NotificationService.IntegrationEvents.Handlers;
 
 public sealed class ReservationCanceledIntegrationEventHandler(
-    ILogger<ReservationCanceledIntegrationEventHandler> logger)
+    ILogger<ReservationCanceledIntegrationEventHandler> logger,
+    IRepository<Notification> notificationRepository,
+    IRepository<NotificationDisabled> notificationDisabledRepository,
+    IHubContext<NotificationHub> hubContext,
+    IUnitOfWork unitOfWork)
     : IIntegrationEventHandler<ReservationCanceledIntegrationEvent>
 {
     public async Task Handle(ReservationCanceledIntegrationEvent @event, CancellationToken ct)
     {
+        var isNotificationDisabled = await notificationDisabledRepository
+            .Query()
+            .Where(x => x.UserId == @event.HostId && x.NotificationType == NotificationType.ReservationCanceled)
+            .AnyAsync(ct);
+        if (isNotificationDisabled)
+        {
+            return;
+        }
         logger.LogInformation("Handling Reservation canceled for HostId={UserId}, ReservationId={ReservationId}",
             @event.HostId, @event.ReservationId);
-        await Task.CompletedTask;
+        var message =
+            $"The Reservation for {@event.AccommodationName} from {@event.StartDate} to {@event.EndDate} was cancelled by {@event.GuestUsername}.";
+        var notification = Notification.Create(@event.HostId, NotificationType.ReservationCanceled, message);
+
+        await notificationRepository.AddAsync(notification);
+
+        await unitOfWork.SaveChangesAsync(ct);
+
+        await hubContext
+            .Clients
+            .User(@event.HostId.ToString())
+            .SendAsync("ReceiveMessage",
+                new NotificationDto
+                (
+                    notification.Id,
+                    NotificationType.ReservationCanceled,
+                    message,
+                    notification.CreatedOn), ct);
     }
 }
--- NotificationService/NotificationService/IntegrationEvents/Handlers/ReservationCreatedIntegrationEventHandler.cs
+++ NotificationService/NotificationService/IntegrationEvents/Handlers/ReservationCreatedIntegrationEventHandler.cs
@@ -1,14 +1,51 @@
-﻿using NotificationService.Common.Events;
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using NotificationService.Common.Events;
+using NotificationService.Common.Hubs;
+using NotificationService.Domain.DTOs;
+using NotificationService.Domain.Entities;
+using NotificationService.Repositories.Interfaces;
 
 namespace NotificationService.IntegrationEvents.Handlers;
 
-public sealed class ReservationCreatedIntegrationEventHandler(ILogger<ReservationCreatedIntegrationEventHandler> logger)
+public sealed class ReservationCreatedIntegrationEventHandler(
+    ILogger<ReservationCreatedIntegrationEventHandler> logger,
+    IRepository<Notification> notificationRepository,
+    IRepository<NotificationDisabled> notificationDisabledRepository,
+    IHubContext<NotificationHub> hubContext,
+    IUnitOfWork unitOfWork)
     : IIntegrationEventHandler<ReservationCreatedIntegrationEvent>
 {
     public async Task Handle(ReservationCreatedIntegrationEvent @event, CancellationToken ct)
     {
+        var isNotificationDisabled = await notificationDisabledRepository
+            .Query()
+            .Where(x => x.UserId == @event.HostId && x.NotificationType == NotificationType.ReservationCreated)
+            .AnyAsync(ct);
+        if (isNotificationDisabled)
+        {
+            return;
+        }
         logger.LogInformation("Handling Reservation created for HostId={UserId}, ReservationId={ReservationId}",
             @event.HostId, @event.ReservationId);
-        await Task.CompletedTask;
+        
+        var message =
+            $"You have a new reservation for {@event.AccommodationName} from {@event.StartDate} to {@event.EndDate}. The reservation was created by {@event.GuestUsername}.";
+        var notification = Notification.Create(@event.HostId, NotificationType.ReservationCreated, message);
+
+        await notificationRepository.AddAsync(notification);
+
+        await unitOfWork.SaveChangesAsync(ct);
+
+        await hubContext
+            .Clients
+            .User(@event.HostId.ToString())
+            .SendAsync("ReceiveMessage",
+                new NotificationDto
+                (
+                    notification.Id,
+                    NotificationType.ReservationCreated,
+                    message,
+                    notification.CreatedOn), ct);
     }
 }
