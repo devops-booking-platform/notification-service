7ba77d7245a75a8ff7090f681b99f193129e8ee4
Nebojša Vuga <72966399+nebojsavuga@users.noreply.github.com>
2026-01-11 12:13:23 +0100

    fix(signlar): Added missing pieces for signalR (#6)

--- NotificationService/NotificationService/Common/Hubs/NotificationHub.cs
+++ NotificationService/NotificationService/Common/Hubs/NotificationHub.cs
@@ -4,6 +4,13 @@ namespace NotificationService.Common.Hubs;
 
 public class NotificationHub : Hub
 {
+    public override Task OnConnectedAsync()
+    {
+        var userId = Context.UserIdentifier;
+        Console.WriteLine($"User connected: {userId}");
+        return base.OnConnectedAsync();
+    }
+    
     public async Task SendMessage(string userId, string message)
     {
         await Clients.User(userId).SendAsync("ReceiveMessage", message);
